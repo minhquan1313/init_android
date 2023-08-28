a1cb31cba14548d9f37c642d71a2cf2fddfe5f44
minhquan1313 <minhquan1313@gmail.com>
2023-08-28 12:24:06 +0700

    done

--- app/src/main/java/com/mtb/myapplication/MainActivity.java
+++ app/src/main/java/com/mtb/myapplication/MainActivity.java
@@ -2,13 +2,26 @@ package com.mtb.myapplication;
 
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements
+        TextToSpeech.OnInitListener {
     TextView hello_text;
+
+    private TextToSpeech tts;
+    private Button buttonSpeak;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +36,20 @@ public class MainActivity extends AppCompatActivity {
     private void bindComponents() {
         hello_text = findViewById(R.id.hello_text);
 
+        tts = new TextToSpeech(this, this);
+        buttonSpeak = findViewById(R.id.button1);
+        editText = findViewById(R.id.editText1);
+
     }
 
     private void bindData() {
+        buttonSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                speakOut();
+            }
 
+        });
         // if (ActivityCompat.checkSelfPermission(this,
         // android.Manifest.permission.BLUETOOTH_CONNECT) !=
         // PackageManager.PERMISSION_GRANTED) {
@@ -51,4 +74,41 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
+
+    @Override
+    public void onDestroy() {
+        // Don't forget to shutdown tts!
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void onInit(int status) {
+
+        if (status == TextToSpeech.SUCCESS) {
+
+            int result = tts.setLanguage(Locale.US);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Toast.makeText(this, "This Language is not supported", Toast.LENGTH_SHORT).show();
+                Log.e("TTS", "This Language is not supported");
+            } else {
+                buttonSpeak.setEnabled(true);
+                speakOut();
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed!");
+        }
+
+    }
+
+    private void speakOut() {
+        String text = editText.getText().toString();
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+    }
+
 }
