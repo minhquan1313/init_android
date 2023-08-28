b72bc9e43e63974610bf14d582db4f5184103230
minhquan1313 <minhquan1313@gmail.com>
2023-08-28 11:51:11 +0700

    done

--- app/src/main/java/com/mtb/myapplication/MainActivity.java
+++ app/src/main/java/com/mtb/myapplication/MainActivity.java
@@ -1,14 +1,116 @@
 package com.mtb.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textview1;
+    private static final int REQUEST_ENABLE_BT = 1;
+    BluetoothAdapter btAdapter;
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textview1 = findViewById(R.id.textView1);
+
+        // Getting the Bluetooth adapter
+        BluetoothManager bluetoothManager = getSystemService(BluetoothManager.class);
+        btAdapter = bluetoothManager.getAdapter();
+        if (btAdapter == null) {
+            // Device doesn't support Bluetooth
+            return;
+        }
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            Utils.askPermission(MainActivity.this, android.Manifest.permission.BLUETOOTH_CONNECT, 1);
+            return;
+        }
+        textview1.append("\nAdapter: " + btAdapter.getName());
+
+        CheckBluetoothState();
+    }
+
+    /* It is called when an activity completes. */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_ENABLE_BT) {
+            CheckBluetoothState();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED)
+            return;
+
+        switch (requestCode) {
+            case 1:
+                textview1.append("\nAdapter: " + btAdapter.getName());
+
+                CheckBluetoothState();
+                break;
+        }
+
+    }
+
+    private void CheckBluetoothState() {
+        // Checks for the Bluetooth support and then makes sure it is turned on
+        // If it isn't turned on, request to turn it on
+        // List paired devices
+        if (btAdapter == null) {
+            textview1.append("\nBluetooth NOT supported. Aborting.");
+            return;
+        } else {
+            if (btAdapter.isEnabled()) {
+                textview1.append("\nBluetooth is enabled...");
+
+                // Listing paired devices
+                textview1.append("\nPaired Devices are:");
+                if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                Set<BluetoothDevice> devices = btAdapter.getBondedDevices();
+                for (BluetoothDevice device : devices) {
+                    textview1.append("\n  Device: " + device.getName() + ", " + device);
+                }
+            } else {
+                // Prompt user to turn on Bluetooth
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
     }
 }
--- app/src/main/java/com/mtb/myapplication/Utils.java
+++ app/src/main/java/com/mtb/myapplication/Utils.java
@@ -1,7 +1,13 @@
+package com.mtb.myapplication;
+
+import android.app.Activity;
+
+import androidx.core.app.ActivityCompat;
+
 public class Utils {
-    public static void askPermission(String permission, int code) {
-        if (!ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, permission)) {
-            requestPermissions(new String[] { permission }, code);
+    public static void askPermission(Activity activity, String permission, int code) {
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+            activity.requestPermissions(new String[]{permission}, code);
         }
     }
 }
