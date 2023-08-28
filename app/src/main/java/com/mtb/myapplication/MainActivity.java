f2ce56ccd2f94ab75ca35020a92fc321fac42162
minhquan1313 <minhquan1313@gmail.com>
2023-08-28 11:58:57 +0700

    update add permission

--- app/src/main/java/com/mtb/myapplication/MainActivity.java
+++ app/src/main/java/com/mtb/myapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.mtb.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindComponents();
+
+        bindData();
+    }
+
+    private void bindComponents() {
+
+    }
+
+    private void bindData() {
+
+        // if (ActivityCompat.checkSelfPermission(this,
+        // android.Manifest.permission.BLUETOOTH_CONNECT) !=
+        // PackageManager.PERMISSION_GRANTED) {
+        // Utils.askPermission(MainActivity.this,
+        // android.Manifest.permission.BLUETOOTH_CONNECT, 1);
+        // return;
+        // }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            @NonNull String[] permissions,
+            @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED)
+            return;
+
+        switch (requestCode) {
+            case 1:
+                break;
+        }
     }
 }
