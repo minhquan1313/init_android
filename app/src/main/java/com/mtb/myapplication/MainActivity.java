fe4aa40ec1524aac6edd91c0f3c650cce6c86cf7
minhquan1313 <minhquan1313@gmail.com>
2023-08-29 19:38:20 +0700

    done

--- app/src/main/java/com/mtb/myapplication/MainActivity.java
+++ app/src/main/java/com/mtb/myapplication/MainActivity.java
@@ -1,14 +1,19 @@
 package com.mtb.myapplication;
 
-import android.content.pm.PackageManager;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    TextView hello_text;
+    TextView textView1;
+    Button exitBtn, traditionalBtn;
+    String[] colors = {"Red", "Orange", "Green"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,34 +26,64 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void bindComponents() {
-        hello_text = findViewById(R.id.hello_text);
-
+        textView1 = findViewById(R.id.textView1);
+        exitBtn = findViewById(R.id.exitBtn);
+        traditionalBtn = findViewById(R.id.traditionalBtn);
     }
 
     private void bindData() {
+        Activity context = MainActivity.this;
 
-        // if (ActivityCompat.checkSelfPermission(this,
-        // android.Manifest.permission.BLUETOOTH_CONNECT) !=
-        // PackageManager.PERMISSION_GRANTED) {
-        // Utils.askPermission(MainActivity.this,
-        // android.Manifest.permission.BLUETOOTH_CONNECT, 1);
-        // return;
-        // }
-    }
+        exitBtn.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
 
-    @Override
-    public void onRequestPermissionsResult(
-            int requestCode,
-            @NonNull String[] permissions,
-            @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            builder.setTitle("Thoát")
+                    .setMessage("Bạn muốn thoát chương trình?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            MainActivity.this.finish();
+                            // txtAlert.setText("YES " + Integer.toString(which));
+                        }
+                    })
+                    .setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // txtAlert.setText("CANCEL " + Integer.toString(which));
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // txtAlert.setText("NO " + Integer.toString(which));
+                        }
+                    })
+                    .create()
+                    .show();
+        });
 
-        if (grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED)
-            return;
+        traditionalBtn.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-        switch (requestCode) {
-            case 1:
-                break;
-        }
+            builder.setTitle("Pick a color").setItems(colors, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    switch (which) {
+                        case 0:
+                            textView1.setBackgroundColor(Color.parseColor("#e74c3c"));
+                            textView1.setTextColor(Color.parseColor("#ecf0f1"));
+                            break;
+                        case 1:
+                            textView1.setBackgroundColor(Color.parseColor("#f39c12"));
+                            textView1.setTextColor(Color.parseColor("#34495e"));
+                            break;
+                        case 2:
+                            textView1.setBackgroundColor(Color.parseColor("#16a085"));
+                            textView1.setTextColor(Color.parseColor("#ecf0f1"));
+                            break;
+                    }
+                }
+            }).create().show();
+        });
     }
 }
