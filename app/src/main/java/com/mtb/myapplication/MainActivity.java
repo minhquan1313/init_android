d92fda7f4b715d44264ed7bd8101d9956c39856c
minhquan1313 <minhquan1313@gmail.com>
2023-08-29 13:15:53 +0700

    up

--- app/src/main/java/com/mtb/myapplication/MainActivity.java
+++ app/src/main/java/com/mtb/myapplication/MainActivity.java
@@ -2,13 +2,15 @@ package com.mtb.myapplication;
 
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
-    TextView hello_text;
+    TextView textView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +22,9 @@ public class MainActivity extends AppCompatActivity {
         bindData();
     }
 
+
     private void bindComponents() {
-        hello_text = findViewById(R.id.hello_text);
+        textView1 = (TextView) findViewById(R.id.textView1);
 
     }
 
@@ -34,6 +37,60 @@ public class MainActivity extends AppCompatActivity {
         // android.Manifest.permission.BLUETOOTH_CONNECT, 1);
         // return;
         // }
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            Utils.askPermission(MainActivity.this, android.Manifest.permission.READ_PHONE_STATE, 1);
+            return;
+        }
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_BASIC_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            Utils.askPermission(MainActivity.this, android.Manifest.permission.READ_BASIC_PHONE_STATE, 2);
+            return;
+        }
+
+        TelephonyManager tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+
+        // Calling the methods of TelephonyManager the returns the information
+        String IMEINumber = tm.getDeviceId();
+        String subscriberID = tm.getDeviceId();
+        String SIMSerialNumber = tm.getSimSerialNumber();
+        String networkCountryISO = tm.getNetworkCountryIso();
+        String SIMCountryISO = tm.getSimCountryIso();
+
+        String softwareVersion = tm.getDeviceSoftwareVersion();
+
+        String voiceMailNumber = tm.getVoiceMailNumber();
+
+        // Get the phone type
+        String strphoneType = "";
+
+        int phoneType = tm.getPhoneType();
+
+        switch (phoneType) {
+            case (TelephonyManager.PHONE_TYPE_CDMA):
+                strphoneType = "CDMA";
+                break;
+            case (TelephonyManager.PHONE_TYPE_GSM):
+                strphoneType = "GSM";
+                break;
+            case (TelephonyManager.PHONE_TYPE_NONE):
+                strphoneType = "NONE";
+                break;
+        }
+
+        // getting information if phone is in roaming
+        boolean isRoaming = tm.isNetworkRoaming();
+
+        String info = "Phone Details:\n";
+        info += "\n IMEI Number:" + IMEINumber;
+        info += "\n SubscriberID:" + subscriberID;
+        info += "\n Sim Serial Number:" + SIMSerialNumber;
+        info += "\n Network Country ISO:" + networkCountryISO;
+        info += "\n SIM Country ISO:" + SIMCountryISO;
+        info += "\n Software Version:" + softwareVersion;
+        info += "\n Voice Mail Number:" + voiceMailNumber;
+        info += "\n Phone Network Type:" + strphoneType;
+        info += "\n In Roaming? :" + isRoaming;
+
+        textView1.setText(info);// displaying the information in the textView
     }
 
     @Override
@@ -51,4 +108,5 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
+
 }
