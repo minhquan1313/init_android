4546ecd6c813a6c27ade6a1d651312432b533a7d
minhquan1313 <minhquan1313@gmail.com>
2023-08-29 12:59:10 +0700

    done

--- app/src/main/java/com/mtb/myapplication/MainActivity.java
+++ app/src/main/java/com/mtb/myapplication/MainActivity.java
@@ -1,14 +1,23 @@
 package com.mtb.myapplication;
 
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
-    TextView hello_text;
+    EditText mobileno, message;
+    Button sendsms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +30,41 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void bindComponents() {
-        hello_text = findViewById(R.id.hello_text);
-
+        mobileno = (EditText) findViewById(R.id.editText1);
+        message = (EditText) findViewById(R.id.editText2);
+        sendsms = (Button) findViewById(R.id.button1);
     }
 
     private void bindData() {
 
-        // if (ActivityCompat.checkSelfPermission(this,
-        // android.Manifest.permission.BLUETOOTH_CONNECT) !=
-        // PackageManager.PERMISSION_GRANTED) {
-        // Utils.askPermission(MainActivity.this,
-        // android.Manifest.permission.BLUETOOTH_CONNECT, 1);
-        // return;
-        // }
+
+        sendsms.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+                    Utils.askPermission(MainActivity.this, android.Manifest.permission.SEND_SMS, 1);
+                    Utils.askPermission(MainActivity.this, Manifest.permission.RECEIVE_SMS, 2);
+                    return;
+                }
+
+                String no = mobileno.getText().toString();
+                String msg = message.getText().toString();
+
+                // Getting intent and PendingIntent instance
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                PendingIntent pi = PendingIntent.getActivity(getApplicationContext(), 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+                // Get the SmsManager instance and call the sendTextMessage method to send
+                // message
+                SmsManager sms = SmsManager.getDefault();
+                sms.sendTextMessage(no, null, msg, pi, null);
+
+                Toast.makeText(getApplicationContext(), "Message Sent successfully!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
     @Override
@@ -51,4 +82,5 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
+
 }
