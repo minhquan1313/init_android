1140e34fa76c76fbf531c1cf523c06a42d864ec5
minhquan1313 <minhquan1313@gmail.com>
2023-08-30 11:38:59 +0700

    done

--- app/src/main/java/com/mtb/myapplication/MainActivity.java
+++ app/src/main/java/com/mtb/myapplication/MainActivity.java
@@ -9,15 +9,26 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
+
 public class MainActivity extends AppCompatActivity {
     Activity activity = MainActivity.this;
     Context context = MainActivity.this;
+    String file = "mtbFile";
+    TextView txt_data1;
+    EditText input_txt1;
+    Button btn_save1, btn_load1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +42,49 @@ public class MainActivity extends AppCompatActivity {
 
     private void bindComponents() {
         // hello_text = findViewById(R.id.hello_text);
-
+        txt_data1 = findViewById(R.id.txt_data1);
+        input_txt1 = findViewById(R.id.input_txt1);
+        btn_save1 = findViewById(R.id.btn_save1);
+        btn_load1 = findViewById(R.id.btn_load1);
     }
 
     private void bindData() {
 
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
             Utils.askPermission(activity, Manifest.permission.INTERNET, 1);
-            return;
         }
+
+        btn_save1.setOnClickListener(view -> {
+            String data = input_txt1.getText().toString();
+            try {
+                FileOutputStream outputStream = openFileOutput(file, MODE_APPEND);
+                outputStream.write(data.getBytes(StandardCharsets.UTF_8));
+                outputStream.close();
+                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_load1.setOnClickListener(view -> {
+            try {
+                FileInputStream inputStream = openFileInput(file);
+
+                int c;
+                StringBuilder temp = new StringBuilder();
+
+                while ((c = inputStream.read()) != -1) {
+                    temp.append((char) c);
+                }
+
+                inputStream.close();
+                txt_data1.setText(temp.toString());
+
+                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
