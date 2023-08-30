e25a146e34fbda529bfab0f6108e98a487e4cec4
minhquan1313 <minhquan1313@gmail.com>
2023-08-30 09:19:58 +0700

    update main

--- app/src/main/java/com/mtb/myapplication/MainActivity.java
+++ app/src/main/java/com/mtb/myapplication/MainActivity.java
@@ -1,14 +1,21 @@
 package com.mtb.myapplication;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Rect;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    TextView hello_text;
+    Activity context = MainActivity.this;
+    // TextView hello_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +28,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void bindComponents() {
-        hello_text = findViewById(R.id.hello_text);
+        // hello_text = findViewById(R.id.hello_text);
 
     }
 
     private void bindData() {
-
         // if (ActivityCompat.checkSelfPermission(this,
         // android.Manifest.permission.BLUETOOTH_CONNECT) !=
         // PackageManager.PERMISSION_GRANTED) {
@@ -34,21 +40,39 @@ public class MainActivity extends AppCompatActivity {
         // android.Manifest.permission.BLUETOOTH_CONNECT, 1);
         // return;
         // }
+
     }
 
     @Override
-    public void onRequestPermissionsResult(
-            int requestCode,
-            @NonNull String[] permissions,
-            @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
         if (grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED)
             return;
 
         switch (requestCode) {
-            case 1:
-                break;
+        case 1:
+            break;
         }
+    }
+
+    /**
+     * Clear focus when tapping outside of any EditText
+     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int) event.getRawX(), (int) event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event);
     }
 }
